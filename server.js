f4a52ee9bb6cdd43205d2e58b60ea5cf865c5bc5
Devina <44644511+Devina6@users.noreply.github.com>
2023-05-27 11:09:54 +0530

    post request, response, body-parsing

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
-//const http = require('http'); not required since app.listen 
-
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req,res,next)=>{
-	console.log("in middleware  1");
-	next();//allows the request to next middleware in line
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/',(req,res,next)=>{
+	//console.log("This always runs");
+	next();
 });
 
-app.use((req,res,next)=>{
-	console.log("in  middleware  2");
+app.use('/add-product',(req,res,next)=>{
+	res.send('<form action="/product" method ="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">ADD Product</button></form>');
+});
+
+app.post('/product',(req,res,next)=>{
+	console.log(req.body);
+	res.redirect('/');
+})
+
+app.use('/',(req,res,next)=>{
 	res.send('<h1>Hello from express</h1>');
 });
-/*const server = http.createServer(app);
 
-server.listen(4000);*/
-//shorter version
+
 
 app.listen(4000);
