5767811d6843d0a7ac70b0ce7897d79be8e938b1
Devina <44644511+Devina6@users.noreply.github.com>
2023-05-28 19:22:11 +0530

    Create message.js

---
+++ views/message.js
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/main.css">
+   </head>
+   <body>
+       <nav class="navbar navbar-expand-lg bg-body-tertiary">
+           <div class="container-fluid">
+               <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="/">SHOP</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="/admin/add-product">ADD Products</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="/contactus/contact">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+		  </div>
+	</nav>
+	<div id="main" class="card card-body">
+            <h1>Form Successfully filled . . . </h1>   
+    </div>	
+</body>
+</html>
