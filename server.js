f752aface1491d5673a44b7b4fd1663b7e11a557
Devina <44644511+Devina6@users.noreply.github.com>
2023-05-28 19:19:49 +0530

    links to contactUs and message

--- server.js
+++ server.js
@@ -5,12 +5,18 @@ const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const contactRoutes = require('./routes/contactus');
+const messageRoutes = require('./routes/message');
+
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/admin',adminRoutes);
-app.use('/shop',shopRoutes);
+app.use(shopRoutes);
+app.use('/contactus',contactRoutes);
+app.use('/message',messageRoutes);
+
 
 app.use((req,res,next)=>{
 	res.status(404).sendFile(path.join(__dirname,'views','404.html'));
