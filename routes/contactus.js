cbb5122a5745b982477f0549edd3aceeb97c0e69
Devina <44644511+Devina6@users.noreply.github.com>
2023-05-30 14:00:11 +0530

    included controller

--- routes/contactus.js
+++ routes/contactus.js
@@ -1,17 +1,10 @@
-const express = require('express');
 const router = express.Router();
-const path = require('path');
+const express = require('express');
+const contactController = require('../controllers/contact');
 
-const rootDir = require('../helpers/path');
+router.get('/contact',contactController.getContact);
 
-router.get('/contact',(req,res,next)=>{
-	res.sendFile(path.join(rootDir,'views','contactus.html'));
-});
-
-router.post('/contact',(req,res,next)=>{
-	console.log(req.body);
-	res.redirect('/message/message');
-});
+router.post('/contact',contactController.postContact);
 
 
 module.exports = router;
