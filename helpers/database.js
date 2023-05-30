f775c1478e4034f691bef234eb159b663bbf3a85
Devina <44644511+Devina6@users.noreply.github.com>
2023-05-30 13:28:12 +0530

    change database root password

--- helpers/database.js
+++ helpers/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host:'localhost',
     user:'root',
     database:'node-complete',
-    password:'libradevina6'
+    password:'sqlpassword'
 });
 
 module.exports = pool.promise();
