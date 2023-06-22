2fb73aac0b63633e08d9f30644ffa3f1b794f597
Devina <44644511+Devina6@users.noreply.github.com>
2023-06-22 10:15:03 +0530

    Update database.js

--- helpers/database.js
+++ helpers/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host:'localhost',
     user:'root',
     database:'node-complete',
-    password:'sqlpassword'
+    password:'SQLpassword'
 });
 
 module.exports = pool.promise();
