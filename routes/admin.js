ad9b59c752357199e38d5c16440616c51cdc8601
Devina <44644511+Devina6@users.noreply.github.com>
2023-06-23 21:42:59 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const adminController = require('../controllers/admin');
 
 
 router.get('/add-product',productsController.getAddProduct);
-
+router.get('/products',adminController.getProducts);
 router.post('/add-product',productsController.postAddProduct);
 
 
