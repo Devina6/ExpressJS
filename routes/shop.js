41145b18d64602cad9fb2bb3316ef6c322cbf1b4
Devina <44644511+Devina6@users.noreply.github.com>
2023-06-24 19:24:53 +0530

    Update shop.js

--- routes/shop.js
+++ routes/shop.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const shopController = require('../controllers/shop');
 
 
-router.get('/',shopController.getProduct);
+router.get('/',shopController.getIndex);
 
 router.get('/products',shopController.getProducts);
 
 router.get('/cart',shopController.getCart);
+
+router.get('/orders',shopController.getOrders);
 
 router.get('/checkout',shopController.getCheckOut);
 
