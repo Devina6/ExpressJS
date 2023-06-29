d405fb6dd8a76f4e0ddb108e2a70651c43ef0b47
Devina <44644511+Devina6@users.noreply.github.com>
2023-06-29 19:21:05 +0530

    edit/update product data

--- controllers/admin.js
+++ controllers/admin.js
@@ -69,7 +69,9 @@ function generateTableBody(products) {
                 <td>${product.title}</td>
                 <td>${product.price}</td>
                 <td>${product.description}</td>
-		<td><a href="/admin/edit-product" id="edit" class="btn btn-info">Edit</a></td>
+		<td><form action="/admin/edit-product/${product.id}" method="GET">
+                    <input type="submit" class="btn btn-info" id="edit" value="Edit">
+                    </form></td>
                 <td><form action="/admin/delete-product"method="POST>"
                     <input type="submit" class="btn btn-danger" id="delete">Delete</button>
                     </form></td>
@@ -79,3 +81,56 @@ function generateTableBody(products) {
 
     return html;
 }
+
+exports.getEditProduct = (req, res, next) => {
+    /*const editMode = req.query.edit;  //?edit=true
+    if(!editMode){
+        return res.redirect('/');
+    }*/
+    const prodId = req.params.ProductId;
+    //Product.findById(prodId)
+    Product.findAll({where:{id:prodId}})
+        .then(products => {
+        if(!products){
+            return res.redirect('/');
+        }
+        fs.readFile(path.join(rootDir, 'views', 'admin/edit-product.html'), 'utf8', (err, data) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send('Internal Server Error');
+            } else {
+                const html = data.replace('<!--TITLE_VALUE-->', products[0].title)
+                           .replace('<!--PRICE_VALUE-->', products[0].price)
+                           .replace('<!--DESCRIPTION_VALUE-->', products[0].description);
+                res.send(html);
+                }
+            });
+
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        });
+};
+
+
+exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedDesc = req.body.description;
+    Product.findById(prodId)
+        .then(product =>{
+            product.title = updatedTitle;
+            product.price = updatedPrice;
+            product.description = updatedDesc;
+            return product.save();
+        })
+        .then(result=>{
+            console.log("updated product");
+            res.redirect('/admin/products');
+        })
+        .catch(err=>console.log(err))
+    
+  };
+
