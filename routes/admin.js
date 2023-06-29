db0578bb8c7ab18852ff250787f0abb75491c3f3
Devina <44644511+Devina6@users.noreply.github.com>
2023-06-29 19:19:02 +0530

    route to edit product

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,7 @@ const adminController = require('../controllers/admin');
 router.get('/add-product',adminController.getAddProduct);
 router.get('/products',adminController.getProducts);
 router.post('/add-product',adminController.postAddProduct);
-
+router.get('/edit-product/:ProductId',adminController.getEditProduct);
+router.post('/edit-product', adminController.postEditProduct);
 
 module.exports = router;
